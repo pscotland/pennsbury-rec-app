f380284b9f19f4a80bf04b78805f6d33c83fd9e3
Patrick Scotland <patrickscotland@yahoo.com>
2026-08-07 00:24:28 -0400

    Add primeNg

--- frontend/public-site/src/app/app.config.ts
+++ frontend/public-site/src/app/app.config.ts
@@ -1,20 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withFetch()),
-    provideAnimationsAsync(),
+    provideHttpClient(),
     provideRouter(
       routes,
       withInMemoryScrolling({
         scrollPositionRestoration: 'top'
       })
-    )
+    ),
+    providePrimeNG({
+      ripple: true,
+      theme: { preset: Aura }
+    })
   ]
 };
